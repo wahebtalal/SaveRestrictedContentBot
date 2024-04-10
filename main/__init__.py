2302d07ce4566d101a6b88449e879aed15b1f9a5
Tahan <wahebtalal@gmail.com>
2024-04-11 01:05:25 +0300

    edit config

--- main/__init__.py
+++ main/__init__.py
@@ -6,18 +6,18 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 from decouple import config
-import logging, time, sys
+import logging, time, sys ,os
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = os.getenv("API_ID", default=None, cast=int)
+API_HASH = os.getenv("API_HASH", default=None)
+BOT_TOKEN = os.getenv("BOT_TOKEN", default=None)
+SESSION = os.getenv("SESSION", default=None)
+FORCESUB = os.getenv("FORCESUB", default=None)
+AUTH = os.getenv("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
